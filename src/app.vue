805659abfce6d6b2d7781bf3113c467207e28889
Wunworld <Wunworld@163.com>
2018-04-15 23:16:40 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">{{msg}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "abssdsasdasdalksjdalkdjds"
+            }
+        }
+    }
+</script>
+
+<style>
+.app{color: deepskyblue;}
+</style>
